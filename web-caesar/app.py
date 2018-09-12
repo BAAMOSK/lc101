073223440f61d4a7c93e5ae9e9b5dc6198f16228
baamosk <bunth667@gmail.com>
2018-09-12 17:50:46 -0400

    working on passing data from form to server

--- web-caesar/app.py
+++ web-caesar/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
 from caesar import rotate_character
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST"])
-def index(rot, text):
-    rot = int(rot)
-    text = chr(text)
-    encrypted = text(rotate_character)
-    return render_template("index.html", text=encrypted)
+@app.route('/', methods=["GET", "POST"])
+def index():
+    data = request.form.get('rot');
+    print(data)
+    #rot = int(rot)
+    #text = chr(text)
+    #encrypted = text(rotate_character)
+    return render_template("index.html", data=data)
+
 
 if( __name__ == "__main__"):
     app.run()
