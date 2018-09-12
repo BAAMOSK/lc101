6915864bc69a2900e5db683fbb80185a3fae2046
baamosk <bunth667@gmail.com>
2018-09-12 02:15:15 -0400

    importing rotate character function

--- web-caesar/app.py
+++ web-caesar/app.py
@@ -4,8 +4,11 @@ from caesar import rotate_character
 app = Flask(__name__)
 
 @app.route('/', methods=["POST"])
-def index():
-    return render_template("index.html")
+def index(rot, text):
+    rot = int(rot)
+    text = chr(text)
+    encrypted = text(rotate_character)
+    return render_template("index.html", text=encrypted)
 
 if( __name__ == "__main__"):
     app.run()
